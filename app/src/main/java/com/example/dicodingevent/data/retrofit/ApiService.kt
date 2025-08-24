7728fe694d171eacef92e96c034cb45478d62318
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-08-24 13:54:40 +0700

    api service dan api config

--- app/src/main/java/com/example/dicodingevent/data/retrofit/ApiService.kt
+++ app/src/main/java/com/example/dicodingevent/data/retrofit/ApiService.kt
@@ -1,4 +1,16 @@
 package com.example.dicodingevent.data.retrofit
 
-class ApiService {
+import com.example.dicodingevent.data.respone.EventResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("events")
+    suspend fun getUpcomingEvents(@Query("active") active: Int): EventResponse
+
+    @GET("events")
+    suspend fun getFinishedEvents(@Query("active") active: Int): EventResponse
+
+    @GET("events?active=-1&limit=1")
+    suspend fun getLatestEvent(): EventResponse
 }
