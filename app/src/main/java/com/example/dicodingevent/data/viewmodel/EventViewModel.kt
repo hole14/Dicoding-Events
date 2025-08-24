7c81b3ecb6a9f807a2b1fa040b80bff46509b0b2
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-08-24 21:09:01 +0700

    view model

--- app/src/main/java/com/example/dicodingevent/data/viewmodel/EventViewModel.kt
+++ app/src/main/java/com/example/dicodingevent/data/viewmodel/EventViewModel.kt
@@ -1,4 +1,25 @@
 package com.example.dicodingevent.data.viewmodel
 
-class EventViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.dicodingevent.data.entity.EventEntity
+import com.example.dicodingevent.data.repository.EventRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class EventViewModel(private val eventRepository: EventRepository): ViewModel() {
+    fun getUpcomingEvents() = eventRepository.getUpcomingEvents()
+    fun getFinishedEvents() = eventRepository.getFinishedEvents()
+
+    private val _query = MutableLiveData<List<EventEntity>>()
+    val query: LiveData<List<EventEntity>> get() = _query
+
+    fun searchEvents(query: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val result = eventRepository.searchEvent(query)
+            _query.postValue(result)
+        }
+    }
 }
