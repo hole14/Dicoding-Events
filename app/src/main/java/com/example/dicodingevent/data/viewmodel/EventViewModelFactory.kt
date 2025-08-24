ec6c4896ce08dbec5e468a913d5db4ab304c6929
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-08-24 21:28:52 +0700

    view model

--- app/src/main/java/com/example/dicodingevent/data/viewmodel/EventViewModelFactory.kt
+++ app/src/main/java/com/example/dicodingevent/data/viewmodel/EventViewModelFactory.kt
@@ -1,4 +1,24 @@
 package com.example.dicodingevent.data.viewmodel
 
-class EventViewModelFactory {
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.dicodingevent.data.di.Injection
+import com.example.dicodingevent.data.repository.EventRepository
+
+class EventViewModelFactory private constructor(private val eventRepository: EventRepository): ViewModelProvider.NewInstanceFactory() {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(EventViewModel:: class.java)) {
+            return EventViewModel(eventRepository) as T
+        }
+        throw IllegalArgumentException("Unknouwn ViewModel class" + modelClass.name)
+    }
+    companion object{
+        @Volatile
+        private var instance: EventViewModelFactory? = null
+        fun getInstance(context: Context): EventViewModelFactory =
+            instance ?: synchronized(this) {
+                instance ?: EventViewModelFactory(Injection.provideRepository(context))
+            }.also { instance = it }
+    }
 }
